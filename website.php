0d59270650dee122861c485e4ae16221d5d4fb07
chansoen <chansoen@students.cs.ubc.ca>
2024-03-28 00:14:15 -0700

    More formatting changes, better organization.

--- website.php
+++ website.php
@@ -40,6 +40,140 @@ $postInsert = 'insert';
 $postUpdate = 'update';
 $getCount = 'count';
 $getDisplay = 'display';
+
+// Other functions
+function debugAlertMessage($message)
+{
+	global $show_debug_alert_messages;
+	if ($show_debug_alert_messages) {
+		echo "<script type='text/javascript'>alert('" . $message . "');</script>";
+	}
+}
+
+function executePlainSQL($cmdstr)
+{ //takes a plain (no bound variables) SQL command and executes it
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = oci_parse($db_conn, $cmdstr);
+	//There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); // For oci_parse errors pass the connection handle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+	$r = oci_execute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For oci_execute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+	return $statement;
+}
+
+function executeBoundSQL($cmdstr, $list)
+{
+	/* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
+							  In this case you don't need to create the statement several times. Bound variables cause a statement to only be
+							  parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
+							  See the sample code below for how this function is used */
+	global $db_conn, $success;
+	$statement = oci_parse($db_conn, $cmdstr);
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+	foreach ($list as $tuple) {
+		foreach ($tuple as $bind => $val) {
+			//echo $val;
+			//echo "<br>".$bind."<br>";
+			oci_bind_by_name($statement, $bind, $val);
+			unset($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+		}
+		$r = oci_execute($statement, OCI_DEFAULT);
+		if (!$r) {
+			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+			$e = OCI_Error($statement); // For oci_execute errors, pass the statementhandle
+			echo htmlentities($e['message']);
+			echo "<br>";
+			$success = False;
+		}
+	}
+}
+
+
+function connectToDB()
+{
+	global $db_conn;
+	global $config;
+	// Your username is ora_(CWL_ID) and the password is a(student number). For example,
+	// ora_platypus is the username and a12345678 is the password.
+	// $db_conn = oci_connect("ora_cwl", "a12345678", "dbhost.students.cs.ubc.ca:1522/stu");
+	$db_conn = oci_connect($config["dbuser"], $config["dbpassword"], $config["dbserver"]);
+	if ($db_conn) {
+		debugAlertMessage("Database is Connected");
+		return true;
+	} else {
+		debugAlertMessage("Cannot connect to Database");
+		$e = OCI_Error(); // For oci_connect errors pass no handle
+		echo htmlentities($e['message']);
+		return false;
+	}
+}
+
+function disconnectFromDB()
+{
+	global $db_conn;
+	debugAlertMessage("Disconnect from Database");
+	oci_close($db_conn);
+}
+
+// HANDLE ALL POST ROUTES
+// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
+function handlePOSTRequest()
+{
+	global $postReset;
+	global $postUpdate;
+	global $postInsert;
+	if (connectToDB()) {
+		switch ($_POST['postAction']) {
+			case $postReset:
+				handleResetRequest();
+				break;
+			case $postUpdate:
+				handleUpdateRequest();
+				break;
+			case $postInsert:
+				handleInsertRequest();
+				break;
+		}
+		disconnectFromDB();
+	}
+}
+
+// HANDLE ALL GET ROUTES
+// A better coding practice is to have one method that reroutes your requests accordingly.
+// It will make it easier to add/remove functionality.
+function handleGETRequest()
+{
+	global $getCount;
+	global $getDisplay;
+	if (connectToDB()) {
+		switch ($_GET['getAction']) {
+			case $getCount:
+				handleCountRequest();
+				break;
+			case $getDisplay:
+				handleDisplayRequest();
+				break;
+		}
+		disconnectFromDB();
+	}
+}
 ?>
 
 <html>
@@ -156,143 +290,7 @@ $getDisplay = 'display';
 	}
 	?>
 
-
-
-
-
-
 	<?php
-	// Other functions
-	
-	function debugAlertMessage($message)
-	{
-		global $show_debug_alert_messages;
-		if ($show_debug_alert_messages) {
-			echo "<script type='text/javascript'>alert('" . $message . "');</script>";
-		}
-	}
-
-	function executePlainSQL($cmdstr)
-	{ //takes a plain (no bound variables) SQL command and executes it
-		//echo "<br>running ".$cmdstr."<br>";
-		global $db_conn, $success;
-		$statement = oci_parse($db_conn, $cmdstr);
-		//There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-	
-		if (!$statement) {
-			echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-			$e = OCI_Error($db_conn); // For oci_parse errors pass the connection handle
-			echo htmlentities($e['message']);
-			$success = False;
-		}
-		$r = oci_execute($statement, OCI_DEFAULT);
-		if (!$r) {
-			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-			$e = oci_error($statement); // For oci_execute errors pass the statementhandle
-			echo htmlentities($e['message']);
-			$success = False;
-		}
-		return $statement;
-	}
-
-	function executeBoundSQL($cmdstr, $list)
-	{
-		/* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
-						 In this case you don't need to create the statement several times. Bound variables cause a statement to only be
-						 parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
-						 See the sample code below for how this function is used */
-		global $db_conn, $success;
-		$statement = oci_parse($db_conn, $cmdstr);
-		if (!$statement) {
-			echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-			$e = OCI_Error($db_conn);
-			echo htmlentities($e['message']);
-			$success = False;
-		}
-		foreach ($list as $tuple) {
-			foreach ($tuple as $bind => $val) {
-				//echo $val;
-				//echo "<br>".$bind."<br>";
-				oci_bind_by_name($statement, $bind, $val);
-				unset($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-			}
-			$r = oci_execute($statement, OCI_DEFAULT);
-			if (!$r) {
-				echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-				$e = OCI_Error($statement); // For oci_execute errors, pass the statementhandle
-				echo htmlentities($e['message']);
-				echo "<br>";
-				$success = False;
-			}
-		}
-	}
-
-
-	function connectToDB()
-	{
-		global $db_conn;
-		global $config;
-		// Your username is ora_(CWL_ID) and the password is a(student number). For example,
-		// ora_platypus is the username and a12345678 is the password.
-		// $db_conn = oci_connect("ora_cwl", "a12345678", "dbhost.students.cs.ubc.ca:1522/stu");
-		$db_conn = oci_connect($config["dbuser"], $config["dbpassword"], $config["dbserver"]);
-		if ($db_conn) {
-			debugAlertMessage("Database is Connected");
-			return true;
-		} else {
-			debugAlertMessage("Cannot connect to Database");
-			$e = OCI_Error(); // For oci_connect errors pass no handle
-			echo htmlentities($e['message']);
-			return false;
-		}
-	}
-
-	function disconnectFromDB()
-	{
-		global $db_conn;
-		debugAlertMessage("Disconnect from Database");
-		oci_close($db_conn);
-	}
-
-	// HANDLE ALL POST ROUTES
-	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
-	function handlePOSTRequest()
-	{
-		global $resetCommand;
-		if (connectToDB()) {
-			switch ($_POST['postAction']) {
-				case $resetCommand:
-					handleResetRequest();
-					break;
-				case 'update':
-					handleUpdateRequest();
-					break;
-				case 'insert':
-					handleInsertRequest();
-					break;
-			}
-			disconnectFromDB();
-		}
-	}
-
-	// HANDLE ALL GET ROUTES
-	// A better coding practice is to have one method that reroutes your requests accordingly.
-	// It will make it easier to add/remove functionality.
-	function handleGETRequest()
-	{
-		if (connectToDB()) {
-			switch ($_GET['getAction']) {
-				case 'count':
-					handleCountRequest();
-					break;
-				case 'display':
-					handleDisplayRequest();
-					break;
-			}
-			disconnectFromDB();
-		}
-	}
-
 	if (isset($_POST['postAction'])) {
 		handlePOSTRequest();
 	} else if (
@@ -300,8 +298,6 @@ $getDisplay = 'display';
 	) {
 		handleGETRequest();
 	}
-
-	// End PHP parsing and send the rest of the HTML content
 	?>
 </body>
 
